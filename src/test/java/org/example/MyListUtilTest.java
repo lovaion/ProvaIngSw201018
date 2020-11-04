6b1683a0b3202d08b12ac781f1f8b68ed30d43f6
lovaion <67899461+lovaion@users.noreply.github.com>
2020-11-04 11:59:10 +0100

    First commit

--- src/test/java/org/example/MyListUtilTest.java
+++ src/test/java/org/example/MyListUtilTest.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import org.joda.time.DateTime;
+import org.joda.time.JodaTimePermission;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
@@ -14,10 +19,12 @@ import static org.junit.Assert.assertNotEquals;
 public class MyListUtilTest {
 
     private static MyListUtil myList;
-
     @BeforeClass
     public static void prepare(){
         myList = new MyListUtil();
+        DateTime dt = new DateTime();
+        System.out.print("Test executed on date: ");
+        System.out.println(dt.toLocalDateTime());
     }
 
     @Test
@@ -48,5 +55,11 @@ public class MyListUtilTest {
     assertEquals(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99), myList.ordinaCrescente(test));
 }
 
+    @AfterClass
+    public static void ending(){
+        DateTime dt = new DateTime();
+        System.out.print("Tests ended on date: ");
+        System.out.println(dt.toLocalDateTime());
+    }
 
 }
